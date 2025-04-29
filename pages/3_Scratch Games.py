1a840be969440b159ca3e11b6dfd6ed7a7687ac5
guitarman123 <westjadie@yahoo.com>
2025-04-29 17:49:26 -0500

    Update 3_Scratch Games.py

--- pages/3_Scratch Games.py
+++ pages/3_Scratch Games.py
@@ -5,3 +5,6 @@ st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layo
 st.title('Here\'s Some of my Projects From Another Website Called Scratch')
 
 st.write('[Here\'s A Simple Platformer](https://scratch.mit.edu/projects/1055346151/)')
+st.write('[An Astroids Spin Off](https://scratch.mit.edu/projects/1056045811/)')
+st.write('[Spy Bot](https://scratch.mit.edu/projects/1054976336/)')
+st.write('[3d maze thing](https://scratch.mit.edu/projects/1063979142/)')
