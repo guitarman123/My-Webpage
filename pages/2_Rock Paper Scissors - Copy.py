258af4515c75a7bcc08ba356b98d7f6dc713bb71
guitarman123 <westjadie@yahoo.com>
2025-04-30 09:31:40 -0500

    Delete pages/2_Rock Paper Scissors - Copy.py

--- pages/2_Rock Paper Scissors - Copy.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
-
-st.title('Rock Paper Scissors!')
-st.write('See if you can beat the computer in a game of ROCK PAPER SCISSORS')
