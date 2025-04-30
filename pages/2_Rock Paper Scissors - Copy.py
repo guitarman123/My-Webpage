e71be4683ea4df79142a2a11570f3a2c282a8db4
guitarman123 <westjadie@yahoo.com>
2025-04-30 09:30:55 -0500

    Add files via upload

---
+++ pages/2_Rock Paper Scissors - Copy.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
+
+st.title('Rock Paper Scissors!')
+st.write('See if you can beat the computer in a game of ROCK PAPER SCISSORS')
