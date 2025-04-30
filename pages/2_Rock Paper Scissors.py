d8eab1b1f7817eb8969603997d4f491fe7f93d17
guitarman123 <westjadie@yahoo.com>
2025-04-30 09:25:44 -0500

    Delete pages/2_Rock Paper Scissors.py

--- pages/2_Rock Paper Scissors.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
-st.write('There will be updates almost daily I\'ll remove this page when i\'ts done.')
