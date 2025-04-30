e01e10680dc277e52ebb9ce1b2e224f269c4d150
guitarman123 <westjadie@yahoo.com>
2025-04-30 09:29:12 -0500

    Update 2_Rock Paper Scissors.py

--- pages/2_Rock Paper Scissors - Copy.py
+++ pages/2_Rock Paper Scissors - Copy.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
 
-st.title('Here\'s Some of my Projects From Another Website Called Scratch')
-
-st.write('[Here\'s A Simple Platformer](https://scratch.mit.edu/projects/1055346151/)')
+st.title('Rock Paper Scissors!')
+st.write('See if you can beat the computer in a game of ROCK PAPER SCISSORS')
