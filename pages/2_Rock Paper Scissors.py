324631a37d914660e805b47e01aafbb497424f40
guitarman123 <westjadie@yahoo.com>
2025-04-30 09:20:11 -0500

    Delete pages/2_Rock Paper Scissors.py

--- pages/2_Rock Paper Scissors.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title = 'Rock Paper Scissors', page_icon = '🎮', layout = 'wide')
-
-st.title('Rock Paper Scissors')
-st.write('Play against the computer to see if you can win!')
-choice = st.radio('Pick one', ['Rock','Paper','Scissors'])
-
