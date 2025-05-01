cac9f44136692592b72b93700ec3fd4bc451af99
guitarman123 <westjadie@yahoo.com>
2025-05-01 12:34:52 -0500

    Add files via upload

--- pages/2_Number guesser.py
+++ pages/2_Number guesser.py
@@ -3,6 +3,13 @@ import random
 
 st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
 
+number = random.randint(1, 100)
 st.title('Number Guessing Game!')
 st.write('Use the slider to choose a number and press check see if you\'re right')
 user_choice = st.slider('Pick a number', 0, 100)
+pressed = st.button
+if pressed:
+    if user_choice == number:
+        st.write('You guessed the number!')
+    else:
+        st.write('Try again.')
