23f06c918557cb069e29311b800745aaefa80ca8
Guitarman <westjadie@yahoo.com>
2025-05-01 18:59:18 -0500

    Add files via upload

---
+++ pages/5_Rock Paper Scissors.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import random
+
+st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
+
+def game():
+    user_choice = st.selectbox("Choose your move:", ["Rock", "Paper", "Scissors"])
+
+    if st.button('Play'):
+        computer = random.choice('r','p','s')
+        if computer == user_choice:
+            st.write('Tie')
+        elif computer == 'r' and user_choice == 'p':
+            st.write('You win!')
+        elif computer == 'p' and user_choice == 's':
+            st.write('You win!')
+        elif computer == 's' and user_choice == 'r':
+            st.write('You win!')
+
+        else:
+            st.write('You lost!')
