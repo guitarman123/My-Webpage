7fec3347663e3f8dc875e9e4ff9b307bd0447888
guitarman123 <westjadie@yahoo.com>
2025-05-01 12:45:03 -0500

    Add files via upload

--- pages/2_Number guesser.py
+++ pages/2_Number guesser.py
@@ -3,15 +3,18 @@ import random
 
 st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
 
-number = random.randint(1, 100)
+
 st.title('Number Guessing Game!')
 st.write('Use the slider to choose a number and press check see if you\'re right')
-user_choice = st.slider('Pick a number', 0, 100)
-pressed = st.button('check')
-if pressed:
-    if user_choice == number:
-        st.write('You guessed the number!')
-    elif user_choice > number:
-        st.write('Too big guess a smaller number.')
-    elif user_choice < number:
-        st.write('Too small guess a bigger number.')
+pressed1 = st.button('Start')
+if pressed1:
+    number = random.randint(1, 100)
+    user_choice = st.slider('Pick a number', 0, 100)
+    pressed = st.button('check')
+    if pressed:
+        if user_choice == number:
+            st.write('You guessed the number!')
+        elif user_choice > number:
+            st.write('Too big guess a smaller number.')
+        elif user_choice < number:
+            st.write('Too small guess a bigger number.')
