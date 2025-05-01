2348d741a5d1b271eb15ef88d0c03648603a04b8
guitarman123 <westjadie@yahoo.com>
2025-05-01 13:02:00 -0500

    Add files via upload

---
+++ pages/2_Number Guesser.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import random
+
+st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
+
+number = random.randint(0, 20)
+
+while True:
+    guess = st.number_input('Pick a number.', 0, 20)
+    if guess == number:
+        st.write_stream('You guessed the number!')
+    if guess < number:
+        st.write_stream('Too low.')
+    if guess > number:
+        st.write_stream('Too high.')
