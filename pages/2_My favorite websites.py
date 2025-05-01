8a05f5c46f2c9bf3642c5c9340080d0341e23d28
guitarman123 <westjadie@yahoo.com>
2025-05-01 17:19:48 -0500

    Add files via upload

---
+++ pages/2_My favorite websites.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layout = 'wide')
+
+st.title('Quick Links to my favorite websites.')
+
+st.write('[Marty Music beginner lessons](https://www.youtube.com/playlist?list=PLP3kvqg_Ut84SdgE4V0Cn6Hb7Rvxrd_A3)')
+st.write('[Marty Music](https://www.martymusic.com/)')
+st.write('[Andy Guitar](https://www.andyguitar.co.uk/)')
+st.write('[]()')
