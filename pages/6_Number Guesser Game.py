f05f856908e662273a59ff108127afd3d475d53c
Guitarman <westjadie@yahoo.com>
2025-05-02 11:19:34 -0500

    Add files via upload

--- pages/6_Number Guesser Game.py
+++ pages/6_Number Guesser Game.py
@@ -7,17 +7,17 @@ st.set_page_config(page_title = 'K\'s Python Projects', page_icon = '🎸', layo
 st.title('Number guessing game.')
 st.write('Use the slider below to pick a number then press check to see if your right!')
 
-
+number = random.randrange(1, 10)
 def game():
-    user_choice = st.number_input('Pick a Number:', 1, 10)
-    number = random.randrange(1, 10)
-    
-    if st.button('Check'):
-        if user_choice == number:
-            st.write('You win!')
-        if user_choice > number:
-            st.write(f'Too big!{number}')
-        if user_choice < number:
-            st.write(f'Too small!{number}')
+    while True:
+        user_choice = st.number_input('Pick a Number:', 1, 10)
+
+        if st.button('Check'):
+            if user_choice == number:
+                st.write('You win!')
+            if user_choice > number:
+                st.write(f'Too big!{number}')
+            if user_choice < number:
+                st.write(f'Too small!{number}')
 
 game()
